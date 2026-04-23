166ccc38d08fa9cad26802132150d7f8498248db
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-23 16:01:32 +0700

    feat(3.FindEvenIndex): solution 1

--- src/LiveCodingTraining/3.FindEvenIndex.cs
+++ src/LiveCodingTraining/3.FindEvenIndex.cs
@@ -10,6 +10,25 @@ public static partial class LiveCodingPractice
     /// </summary>
     public static int FindEvenIndex(int[] arr)
     {
-        throw new NotImplementedException();
+        var arrLength = arr?.Length - 1 ?? throw new ArgumentNullException(nameof(arr));
+
+        if (arrLength == 0)
+        {
+            return 0;
+        }
+
+        for (var i = 1; i < arrLength; ++i)
+        {
+            var leftSum = arr!.Take(i).Sum();
+            var rightSum = arr!.Skip(1 + i).Sum();
+
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+        }
+
+
+        return -1;
     }
 }

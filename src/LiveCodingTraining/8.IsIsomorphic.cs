7272055bdebd6c12d989798fe26ded4508425617
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-27 20:30:47 +0700

    feat(8.IsIsomorphic): solution

--- src/LiveCodingTraining/8.IsIsomorphic.cs
+++ src/LiveCodingTraining/8.IsIsomorphic.cs
@@ -9,6 +9,27 @@ public static partial class LiveCodingPractice
     /// </summary>
     public static bool IsIsomorphic(string s, string t)
     {
-        throw new NotImplementedException();
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        var d = new Dictionary<char, char>();
+
+        for (var i = 0; i < s.Length; ++i)
+        {
+            var c = s[i];
+            var x = t[i];
+            var b1 = d.TryGetValue(c, out var v) && v != x;
+            var b2 = d.ContainsValue(x) && d.First(a => a.Value == x).Key != c;
+            if (b1 || b2)
+            {
+                return false;
+            }
+
+            d[c] = x;
+        }
+
+        return true;
     }
 }

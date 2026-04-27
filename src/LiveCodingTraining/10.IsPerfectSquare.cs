4dca63ad1dc999bce32e66da5db588ecaa0d827b
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-27 22:13:44 +0700

    feat(10.IsPerfectSquare): solution

--- src/LiveCodingTraining/10.IsPerfectSquare.cs
+++ src/LiveCodingTraining/10.IsPerfectSquare.cs
@@ -9,6 +9,39 @@ public static partial class LiveCodingPractice
     /// </summary>
     public static bool IsPerfectSquare(int num)
     {
-        throw new NotImplementedException();
+        if (num <= 0)
+        {
+            return false;
+        }
+
+        if (num == 1)
+        {
+            return true;
+        }
+
+        long left = 2;
+        long right = num / 2;
+
+        while (left <= right)
+        {
+            long target = left + (right - left) / 2;
+            long doubled = target * target;
+
+            if (doubled == num)
+            {
+                return true;
+            }
+
+            if (doubled > num)
+            {
+                right = target - 1;
+            }
+            else
+            {
+                left = target + 1;
+            }
+        }
+
+        return false;
     }
 }

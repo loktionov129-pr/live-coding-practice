42a66fc41e65a8a4ebaffa2da5d3ae0cc2f95795
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-27 22:33:51 +0700

    feat(9.RotateMatrix90Clockwise): solution + test

--- src/LiveCodingTraining/9.RotateMatrix90Clockwise.cs
+++ src/LiveCodingTraining/9.RotateMatrix90Clockwise.cs
@@ -4,10 +4,37 @@ public static partial class LiveCodingPractice
 {
     /// <summary>
     /// Повернуть квадратную матрицу на 90 градусов по часовой стрелке in-place.
-    /// Пример: RotateMatrix90Clockwise(new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } }) -> [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    /// Пример: RotateMatrix90Clockwise(new[] {
+    ///  new[] { 1, 2, 3 },
+    ///  new[] { 4, 5, 6 },
+    ///  new[] { 7, 8, 9 }
+    ///  })
+    ///  ->
+    ///  [[7, 4, 1],
+    ///  [8, 5, 2],
+    ///  [9, 6, 3]]
     /// </summary>
     public static void RotateMatrix90Clockwise(int[][] matrix)
     {
-        throw new NotImplementedException();
+        var len = matrix?.Length ?? throw new ArgumentNullException(nameof(matrix));
+        if (!matrix.All(x => x.Length == len))
+        {
+            throw new ArgumentException("Must be a square", nameof(matrix));
+        }
+
+        for (var i = 0; i < len; ++i)
+        {
+            for (var j = i + 1; j < len; ++j)
+            {
+                var tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        foreach(var row in matrix)
+        {
+            Array.Reverse(row);
+        }
     }
 }
--- tests/LiveCodingTraining.UnitTests/9.RotateMatrix90ClockwiseTests.cs
+++ tests/LiveCodingTraining.UnitTests/9.RotateMatrix90ClockwiseTests.cs
@@ -33,4 +33,42 @@ public class RotateMatrix90ClockwiseTests
 
         Assert.Equal([[42]], matrix);
     }
+
+    [Fact]
+    public void RotateMatrix90Clockwise_With2x3_ThrowsArgumentException()
+    {
+        int[][] matrix = [
+            [1, 2, 3],
+            [4, 6, 6]
+        ];
+
+        var ex = Record.Exception(() => LiveCodingPractice.RotateMatrix90Clockwise(matrix));
+
+        Assert.IsType<ArgumentException>(ex);
+    }
+
+    
+
+    [Fact]
+    public void RotateMatrix90Clockwise_With1x3_2x4_ThrowsArgumentException()
+    {
+        int[][] matrix = [
+            [1, 2, 3],
+            [4, 6, 6, 7]
+        ];
+
+        var ex = Record.Exception(() => LiveCodingPractice.RotateMatrix90Clockwise(matrix));
+
+        Assert.IsType<ArgumentException>(ex);
+    }
+
+    [Fact]
+    public void RotateMatrix90Clockwise_With2x3_ThrowsArgumentNullException()
+    {
+        int[][] matrix = null;
+
+        var ex = Record.Exception(() => LiveCodingPractice.RotateMatrix90Clockwise(matrix));
+
+        Assert.IsType<ArgumentNullException>(ex);
+    }
 }
